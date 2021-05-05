4371eeb23ffe4548d14a0d4dc75dac98139eea44
Sreedhar-R <sreedharrravi97@gmail.com>
2021-05-05 14:40:37 +0530

    added timer

---
+++ src/Inputbox.js
@@ -0,0 +1,17 @@
+import './Inputbox.css';
+
+const Inputbox = ({Inputbox,searchChange,date,days,hours,mins,secs}) => {
+    return(
+        <div className="display">
+            <h3>Timer ends on {date} </h3>
+            <h1>{days}-Days {hours}-Hours {mins}-Mins {secs}-Secs </h1>
+            <div>
+                <input type="text" placeholder="01 January, 2020(format)" onChange={searchChange}/>
+                <button onClick={Inputbox}>submit</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Inputbox;
